b89a5356e5fbabbd2aee4a7d7959d3ae61e633ac
Martin Kalábek <kalabek@ipex.cz>
2017-04-12 15:58:39 +0200

    Simplify config

--- firebase.js
+++ firebase.js
@@ -2,7 +2,10 @@ import Firebase from 'firebase-admin'
 import config from './config.json'
 
 const app = Firebase.initializeApp({
-  credential: Firebase.credential.cert(config.FB_SERVICE_ACCOUNT),
+  credential: Firebase.credential.cert({
+    private_key: config.FB_PRIVATE_KEY || process.env.FB_PRIVATE_KEY,
+    client_email: config.FB_CLIENT_EMAIL || process.env.FB_CLIENT_EMAIL
+  }),
   databaseURL: config.FB_DATABASE_URL,
   // databaseAuthVariableOverride: {
   //   uid: "vps"
